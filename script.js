8a7f0e815247d87408461248bd5b0aa902857772
Gabriella Melo <gabriella.melo0119@gmail.com>
2024-02-18 20:40:38 -0300

    Footer animation fixed

--- script.js
+++ script.js
@@ -4,17 +4,37 @@ document.addEventListener("DOMContentLoaded", function () {
   card.classList.add('active');
 
   function typeWriter(elemento) {
-    const rodapeArray = elemento.innerText.split('');
-    elemento.innerHTML = '';
-    rodapeArray.forEach((letra, i) => {
-      setTimeout(function() {
+
+  const rodapeArray = elemento.innerText.split('');
+
+  elemento.innerHTML = '';
+
+  rodapeArray.forEach((letra, i) => {
+
+    setTimeout(function() {
+
+      console.log(letra);
+      console.log(i);
+
+      if(i == 13) {
+        elemento.innerHTML += `<a href="https://www.frontendmentor.io/" class="frontend">${letra}</a>`;
+      } else if(i > 13 && i < 28) {
+        rodape.querySelector('a.frontend').innerHTML += letra;
+      } else if(i == 38) {
+        elemento.innerHTML += `<a href="https://github.com/GabriellaAM" class="name">${letra}</a>`;
+      } else if(i > 37 && i < 53) {
+        rodape.querySelector('a.name').innerHTML += letra;
+      } else {
         elemento.innerHTML += letra;
-      }, 75 * i)
-    });
-  }
-  
-  const rodape = document.querySelector('.attribution');
-  typeWriter(rodape);
+      }
+    }, 80 * i)
+
+  });
+
+}
+
+const rodape = document.querySelector('.attribution');
+typeWriter(rodape);
 
 });
 
