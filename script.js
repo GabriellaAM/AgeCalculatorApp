dd0f3f0b6ac4bb6900200c0bf181a34eff7be0b8
Gabriella Melo <gabriella.melo0119@gmail.com>
2024-02-18 19:27:15 -0300

    Footer animation added

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let card = document.querySelector('.container');
+document.addEventListener("DOMContentLoaded", function () {
+  let card = document.querySelector('.container');
 
-    card.classList.add('active'); 
+  card.classList.add('active');
 
-  });
+  function typeWriter(elemento) {
+    const rodapeArray = elemento.innerText.split('');
+    elemento.innerHTML = '';
+    rodapeArray.forEach((letra, i) => {
+      setTimeout(function() {
+        elemento.innerHTML += letra;
+      }, 75 * i)
+    });
+  }
+  
+  const rodape = document.querySelector('.attribution');
+  typeWriter(rodape);
+
+});
+
